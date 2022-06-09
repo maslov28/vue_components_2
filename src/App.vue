37ee2c0ea9b7c6ca7a7c34b89880b59bf4d4f68d
maslov28 <artem.fatefulchoice@mail.ru>
2022-06-09 12:15:01 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ul>
+    <li>создать два SFC BaseCard.vue и BaseCards.vue</li>
+    <li>Структура компонентов
+      <ul>
+        <li>App.vue
+          <ul>
+            <li>BaseCards (принимает параметров массив [{id: 1, title: ‘Card1’}, {id: 2, title: ‘Card2’}]) и рендерит в цикле BaseCard
+              <ul>
+                <li>
+                  BaseCard (принимает параметром title и рендерит его)
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+  </ul>
+
+  <base-cards
+    :array="array"
+  ></base-cards>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import BaseCards from './components/BaseCards.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components: {BaseCards},
+  data() {
+    return {
+        array: [
+          {id: 1, title: 'Card1'}, 
+          {id: 2, title: 'Card2'}
+        ]
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/BaseCards.vue
@@ -0,0 +1,16 @@
+<template>
+  <base-card 
+    v-for="item in array" 
+    :key="item.id"
+    :title="item.title"
+  >
+  </base-card>
+</template>
+
+<script>
+import BaseCard from './BaseCard.vue'
+export default {
+  components: { BaseCard },
+  props: ['array']
+}
+</script>
